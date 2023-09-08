58e912c703213cdc309ec8dbc6f6525148361751
michael <newlechim@gmail.com>
2023-09-08 01:51:39 +0100

    Hello World

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints a string using the puts function
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+    char Mystr[] = "\"Programming is like building a multilingual puzzle";
+   
+    puts(Mystr);
+
+    return (0);
+}
+
