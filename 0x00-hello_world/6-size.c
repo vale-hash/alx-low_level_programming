66cc9cc490fd5c8159795ba558a32fe9d5b20834
michael <newlechim@gmail.com>
2023-09-08 02:00:38 +0100

    betty 6-size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
  * main - Entry point
  *
@@ -14,6 +13,5 @@ int main(void)
     printf("Size of a long long int: %ld byte(s)\n", sizeof(long long int));
     printf("Size of a float: %ld byte(s)\n", sizeof(float));
 
-    return 0;
+    return (0);
 }
-
