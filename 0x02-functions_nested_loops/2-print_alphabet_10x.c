166f9ebd0cc119619732644e3461635b814cc8af
Michael Elekwachi <newlechim@gmail.com>
2023-09-12 21:55:39 +0100

    2-print_alphabet_10x.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_10x.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ *print_alphabet_x10 -writes characters a-z with 10 rows to stdout
+ *
+ * Description: Prints the word putchar
+ * Return: Always 0 (Success)
+ */
+int print_alphabet_x10(void)
+{
+int i;
+char c;
+for (c = 97; c < 123; c++)
+{
+_putchar(c);
+for (i = 0; i < 10; i++)
+  {
+_putchar(c);
+  }
+}
+_putchar('\n');
+return (0);
+}
