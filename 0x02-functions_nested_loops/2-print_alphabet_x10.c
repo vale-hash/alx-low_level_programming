6528a476f7947395ea905bbd2f2f8b3eafce0b1c
Michael Elekwachi <newlechim@gmail.com>
2023-09-12 22:13:58 +0100

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,11 +9,11 @@ int print_alphabet_x10(void)
 {
 int i;
 char c;
-for (c = 97; c < 123; c++)
-{
 for (i = 0; i < 10; i++)
+{
+for (c = 97; c < 123; c++)
   {
-_putchar('c');
+_putchar(c);
   }
 _putchar('\n');
 }
