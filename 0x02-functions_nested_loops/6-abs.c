9e9a1f24b5c1854a8b3b2ea5b691dae975419a5c
Michael Elekwachi <newlechim@gmail.com>
2023-09-12 23:48:42 +0100

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ *_abs -prints the absolute value of an integer
+ *@n: - Character that is checked
+ *
+ * Return: 1 if n is positve -1 if negative and 0 if its 0
+ */
+int _abs(int n)
+{
+int x;
+if (n > 0)
+{
+x = (n * -1);
+return (x);
+}
+else if (n < 0)
+{
+x = (n * -1);
+return (x);
+}
+else
+{
+return (0);
+}
+}
