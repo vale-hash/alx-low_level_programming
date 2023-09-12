01a9c8567db8ecefc9545ae5f433b4a77a262598
Michael Elekwachi <newlechim@gmail.com>
2023-09-12 19:29:07 +0100

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Description: Prints the word putchar
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char c;
+for (c = 97; c < 112; c++)
+{
+_putchar(c);
+}
+return (0);
+}
