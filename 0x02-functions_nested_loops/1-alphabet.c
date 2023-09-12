f62a6746ce300016e2c720d1df80b358a54f952a
Michael Elekwachi <newlechim@gmail.com>
2023-09-12 19:35:07 +0100

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
- * main - Entry point
+ * print_alphabet -writes characters a-z to stdout
  *
  * Description: Prints the word putchar
  * Return: Always 0 (Success)
  */
-int main(void)
+int print_alphabet(void)
 {
 char c;
 for (c = 97; c < 112; c++)
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,7 +1,7 @@
 #ifndef HEADER_FILE
 #define HEADER_FILE
 int _putchar(char c);
-
+int print_alphabet(void);
 
 
 #endif
