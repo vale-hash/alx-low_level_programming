4dad239a926af49fa92aeee1bafa3fd6a0a86a07
Michael Elekwachi <newlechim@gmail.com>
2023-09-14 21:26:05 +0100

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *mul -multiplies two integers
+ *
+ *@b: - Second Character to be multiplied
+ *
+ *@a: - First character to be multiplied
+ *
+ * Return: 1 if c is lowercase 0 if its not
+ */
+int mul(int a, int b)
+{
+
+int c;
+c = a * b;
+return (c);
+
+}
