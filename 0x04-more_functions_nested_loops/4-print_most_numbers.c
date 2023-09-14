3fa136cf792bfe1bd90013a34648d4da705db0b1
Michael Elekwachi <newlechim@gmail.com>
2023-09-14 21:43:20 +0100

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 /**
- * print_numbers - function to print 0-9
+ * print_most_numbers - function to print 0-9 with the exception of 2&4
  *
  * Return: 0
  */
-void print_numbers(void)
+void print_most_numbers(void)
 {
 int c;
 for (c = '0'; c <= '9'; c++)
