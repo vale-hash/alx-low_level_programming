e793172aa8db71897a9a431ddf04236520d8fe50
Michael Elekwachi <newlechim@gmail.com>
2023-09-15 01:13:25 +0100

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * print_line - function to print a line as many tim as n
+ *@n: - number of times underscore is gonna be printed
+ * Return: 0
+ */
+void print_line(int n)
+{
+int c;
+for (c = 0; c <= n; c++)
+{
+putchar('_');
+}
+putchar('\n');
+}
