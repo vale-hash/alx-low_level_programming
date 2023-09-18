868612f43958868e563f6c01d91f29a5fc3e2f8f
Michael Elekwachi <newlechim@gmail.com>
2023-09-18 13:51:04 +0100

    simple pointer

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - function to set a the value of a pointer to 98
+ *@n: - number of times underscore is gonna be printed
+ * Return: 0
+ */
+void reset_to_98(int *n)
+{
+*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+this directory is forpointer arraays and strings tasks completed during the ALXSE course 
