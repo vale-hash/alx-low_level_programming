69644a1c271ca975249f39ba2ae4d068bc07f106
Michael Elekwachi <newlechim@gmail.com>
2023-09-18 22:26:24 +0100

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_rev - function to reverse a string
+ *@s: - the string to be printed
+ * Return: 0
+ */
+void print_rev(char *s)
+{
+int i, d;
+char c;
+for (i = 0; s[i] != '\0'; i++)
+{
+
+}
+
+for (d = i; d != 0; d--)
+{
+c = s[d];
+_putchar(c);
+}
+
+_putchar('\n');
+}
