f1b8681ea7fe87c52ab3656673e57716aff2cd93
Michael Elekwachi <newlechim@gmail.com>
2023-09-19 02:01:33 +0100

    strinngg

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -8,11 +8,10 @@ void puts2(char *str)
 {
 int i;
 char c;
-for (i = 0; str[i] != '\0'; i = i + 2)
+for (i = 0; str[i] != '\0'; i++)
 {
-if (str[i] == '\\')
+if (i % 2 == 0)
 {
-break;
 c = str[i];
 _putchar (c);
 }
