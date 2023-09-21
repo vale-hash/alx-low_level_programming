8dadecc616ea9f8691d523f331b3ecac230c32b2
Michael Elekwachi <newlechim@gmail.com>
2023-09-22 00:55:07 +0100

    def don't do my best work at night

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * string_toupper - function to change  lowercase letters to uppercase.
+ * @a: array
+ * Return: string a
+ */
+char *string_toupper(char *a)
+{
+	int i;
+
+	for (i = 0; a[i] != '\0'; i++)
+	{
+		if (a[i] > 96 && a[i] < 123)
+		{
+			a[i] -= 32;
+		}
+	}
+	return (a);
+}
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -2,4 +2,7 @@
 #define HEADER_FILE
 int _putchar(char c);
 char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+char *string_toupper(char *);
+int _strcmp(char *s1, char *s2);
 #endif
