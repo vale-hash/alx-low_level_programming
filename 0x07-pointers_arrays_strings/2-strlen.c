bc0841b15dcefea8aeb92fc9ebf8518fd611547f
Michael Elekwachi <newlechim@gmail.com>
2023-09-26 02:02:36 +0100

    2 and 3

---
+++ 0x07-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _strlen - function to get the lenght of a string
+ *@s: - the string which the lenght would be gotten
+ * Return: 0
+ */
+int _strlen(char *s)
+{
+int i;
+for (i = 0; s[i] != '\0'; i++)
+{
+}
+return (i);
+}
