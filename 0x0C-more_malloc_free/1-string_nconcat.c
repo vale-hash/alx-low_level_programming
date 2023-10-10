9795b51ac05f85d105940f9635bde49db9577332
Michael Elekwachi <newlechim@gmail.com>
2023-10-10 03:53:25 +0100

    fixed

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * malloc_checked - function to allocate memory using malloc
+ * @b: value to create memory for
+ * Return: returns a pointer to the allocated memory
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *p;
+
+p = malloc(b);
+
+	if (p == NULL)
+	{
+		free(p);
+		exit(98);
+	}
+return (p);
+}
---
+++ 0x0C-more_malloc_free/main.h
@@ -0,0 +1,6 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+int _putchar(char c);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+int *array_range(int min, int max);
+#endif
