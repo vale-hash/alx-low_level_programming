30dc19d2214d7086f20bf5f7cba1b877f991ec40
Michael Elekwachi <newlechim@gmail.com>
2023-10-13 01:08:04 +0100

    2-print_strings.c abeg

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -1,10 +1,12 @@
 #include "variadic_functions.h"
 #include <stdarg.h>
+#include <stdlib.h>
 #include <stdio.h>
 /**
- * print_strings - prints strings
- * @separator: pointer param
- * @n: param:wq
+ * print_strings - func that print strings, followed by a new line
+ * @n: Type unsigned int, number of str paseed to a function
+ * @separator: type char pointer string to be printed between numbers
+ * Return: 0
  */
 void print_strings(const char *separator, const unsigned int n, ...)
 {
