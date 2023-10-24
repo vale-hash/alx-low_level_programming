96fb468ecc92799a0b12348aae7cd6a36a1b57a8
Elekwachi Michael <89383831+vale-hash@users.noreply.github.com>
2023-10-24 07:43:48 +0100

    Delete 0x10-variadic_functions directory

--- 0x10-variadic_functions/variadic_functions.h
+++
@@ -1,7 +0,0 @@
-#ifndef VARIADIC_FUNCTIONS_H
-#define VARIADIC_FUNCTIONS_H
-void print_numbers(const char *separator, const unsigned int n, ...);
-int sum_them_all(const unsigned int n, ...);
-void print_strings(const char *separator, const unsigned int n, ...);
-
-#endif
