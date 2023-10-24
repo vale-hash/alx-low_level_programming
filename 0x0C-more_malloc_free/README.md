a1d2131d30a2faf0cc79c4f1aee09c55bdb7559e
Elekwachi Michael <89383831+vale-hash@users.noreply.github.com>
2023-10-24 07:44:13 +0100

    Delete 0x0C-more_malloc_free directory

--- 0x0C-more_malloc_free/README.md
+++
@@ -1,1 +0,0 @@
-learning memory allocation repository
--- 0x0C-more_malloc_free/main.h
+++
@@ -1,6 +0,0 @@
-#ifndef HEADER_FILE
-#define HEADER_FILE
-int _putchar(char c);
-char *string_nconcat(char *s1, char *s2, unsigned int n);
-int *array_range(int min, int max);
-#endif
