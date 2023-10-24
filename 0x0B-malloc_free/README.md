ef647b2f0f79dd596c3a1d7640592876ddf9bc71
Elekwachi Michael <89383831+vale-hash@users.noreply.github.com>
2023-10-24 07:44:01 +0100

    Delete 0x0B-malloc_free directory

--- 0x0B-malloc_free/README.md
+++
@@ -1,1 +0,0 @@
-repo for alx malloc task
--- 0x0B-malloc_free/main.h
+++
@@ -1,7 +0,0 @@
-#ifndef HEADER_FILE
-#define HEADER_FILE
-int _putchar(char c);
-char *str_concat(char *s1, char *s2);
-int **alloc_grid(int width, int height);
-void free_grid(int **grid, int height);
-#endif
