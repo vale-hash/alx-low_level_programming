2a780704a3601f2d8c14e40620b030bc29e62e10
Michael Elekwachi <newlechim@gmail.com>
2023-10-31 20:06:47 +0100

    betty T.T

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -3,8 +3,9 @@
 /**
  * create_array - function to dynamically allocate an array
  *
- * @c:the char to initialize the array
- * @size:the size of the array
+ * @c: the char to initialize the array
+ * @size: the size of the array
+ * Return: s
  */
 
 char *create_array(unsigned int size, char c)
@@ -14,16 +15,16 @@ char *s;
 	if (size == 0)
 	{
 		return (NULL);
-	}	
-	s = malloc(sizeof(char)*size);
+	}
+	s = malloc(sizeof(char) * size);
 		if (s == NULL)
 	{
 		return (NULL);
-	}	
-	for(i = 0; i < size; i++)
+	}
+	for (i = 0; i < size; i++)
 	{
 		s[i] = c;
-	} 
+	}
 
 	return (s);
 }
