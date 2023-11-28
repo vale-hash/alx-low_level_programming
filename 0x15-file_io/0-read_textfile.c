2952266d76f2f1ee67427a3bc8dc0738fee3486c
Michael Elekwachi <newlechim@gmail.com>
2023-11-28 01:08:15 +0100

    file read

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+ * read_textfile - function to reads a text file and prints it to POSIX STDOUT
+ * @filename: name of the file to be read
+ * @letters : the number of letters it should read
+ * Return: returns the count of words
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fp;
+	size_t counter;
+	size_t count;
+	char *buffer;
+
+	buffer = malloc(sizeof(char) * letters);
+
+	if (filename == NULL)
+		return (0);
+	fp = open(filename, O_RDONLY);
+	if (fp != 0)
+		return (0);
+
+	if (buffer == NULL)
+	{
+		close(fp);
+		return (0);
+	}
+
+	count = read(fp, buffer, letters);
+	if (count < 0)
+		return (0);
+
+	counter = write(STDOUT_FILENO, buffer, count);
+
+	free(buffer);
+
+	if (count != counter)
+		return (0);
+
+	return (counter);
+}
